41255fe52c8c6d7d47e3a68ab62fba9e712af7d3
preseverence <rattler@brokenevent.com>
2019-07-16 16:32:37 +0300

    * bump version to 1.1.1 * copyright year update

--- BrokenEvent.ILStrip.CLI/Properties/AssemblyInfo.cs
+++ BrokenEvent.ILStrip.CLI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BrokenEvent.ILStrip.CLI")]
-[assembly: AssemblyCopyright("Copyright (C)2017, Broken Event")]
+[assembly: AssemblyCopyright("©2016-2019 Broken Event. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1")]
